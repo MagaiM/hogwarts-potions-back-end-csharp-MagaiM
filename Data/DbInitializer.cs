1cc7f6c5bb8ff31b78bc1b5e4361621ca05b0bc0
MagaiM <magai.marton@gmail.com>
2022-06-28 20:01:46 +0200

    Add some default data to database on creation

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -1,6 +1,8 @@
 ﻿using HogwartsPotions.Models;
 using System;
 using System.Linq;
+using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Models.Enums;
 
 namespace HogwartsPotions.Data
 
@@ -11,13 +13,69 @@ namespace HogwartsPotions.Data
         {
             context.Database.EnsureCreated();
 
-            // Look for any students.
-            if (context.Students.Any())
+            // Look for any students and rooms.
+            if (context.Students.Any() && context.Rooms.Any())
             {
                 return;   // DB has been seeded
             }
 
+            var rooms = new Room[]
+            {
+                new Room
+                {
+                    Capacity = 10,
+                },
+                new Room
+                {
+                    Capacity = 10,
+                },
+                new Room
+                {
+                    Capacity = 10,
+                },
+                new Room
+                {
+                    Capacity = 10,
+                }
+            };
 
+            foreach (var room in rooms)
+            {
+                context.Rooms.Add(room);
+            }
+            context.SaveChanges();
+
+            var students = new Student[]
+            {
+                new Student
+                {
+                    HouseType = HouseType.Gryffindor,
+                    Name = "Hermione Granger",
+                    PetType = PetType.Cat,
+                    Room = rooms.First(room => room.ID == 1),
+                },
+                new Student
+                {
+                    HouseType = HouseType.Gryffindor,
+                    Name = "Harry Potter",
+                    PetType = PetType.Owl,
+                    Room = rooms.First(room => room.ID == 1),
+                },
+                new Student
+                {
+                    HouseType = HouseType.Slytherin,
+                    Name = "Draco Melfloy",
+                    PetType = PetType.None,
+                    Room = rooms.First(room => room.ID == 2),
+                },
+            };
+
+            foreach (var student in students)
+            {
+                context.Students.Add(student);
+                context.Rooms.First(room => room.ID == student.Room.ID).Residents.Add(student);
+            }
+            context.SaveChanges();
         }
     }
 }
