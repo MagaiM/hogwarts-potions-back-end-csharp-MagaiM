9c5983d941c51f2430b88219c01c1dac29b344eb
MagaiM <magai.marton@gmail.com>
2022-06-28 15:16:17 +0200

    Add DbInitializer

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,23 @@
+﻿using HogwartsPotions.Models;
+using System;
+using System.Linq;
+
+namespace HogwartsPotions.Data
+
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(HogwartsContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any students.
+            if (context.Students.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+
+        }
+    }
+}
