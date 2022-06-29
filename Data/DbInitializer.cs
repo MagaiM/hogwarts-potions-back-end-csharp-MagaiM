d615bb51e317742815d1ebc782db85d94fbe749f
MagaiM <magai.marton@gmail.com>
2022-06-29 14:23:01 +0200

    Add some base ingredients to database on Initialize

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -76,6 +76,63 @@ namespace HogwartsPotions.Data
                 context.Rooms.First(room => room.ID == student.Room.ID).Residents.Add(student);
             }
             context.SaveChanges();
+
+            // Look for any ingredients.
+            if (context.Ingredients.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var ingredients = new Ingredient[]
+            {
+                new Ingredient
+                {
+                    Name = "Vodka"
+                },
+                new Ingredient
+                {
+                    Name = "Beer"
+                },
+                new Ingredient
+                {
+                    Name = "Water"
+                },
+                new Ingredient
+                {
+                    Name = "Rum"
+                },
+                new Ingredient
+                {
+                    Name = "Whisky"
+                },
+                new Ingredient
+                {
+                    Name = "Potato"
+                },
+                new Ingredient
+                {
+                    Name = "Strawberry"
+                },
+                new Ingredient
+                {
+                    Name = "Tomato"
+                },
+                new Ingredient
+                {
+                    Name = "Apple"
+                },
+                new Ingredient
+                {
+                    Name = "Ginseng"
+                },
+
+            };
+
+            foreach (var ingredient in ingredients)
+            {
+                context.Ingredients.Add(ingredient);
+            }
+            context.SaveChanges();
         }
     }
 }
