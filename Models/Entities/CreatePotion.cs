439734db2d5623f17fe3df4123e43473cc6cd7e5
MagaiM <magai.marton@gmail.com>
2022-06-30 19:00:41 +0200

    Add CreatePotion class

---
+++ Models/Entities/CreatePotion.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace HogwartsPotions.Models.Entities
+{
+    public class CreatePotion
+    {
+        public string Name { get; set; }
+        public long StudentId { get; set; }
+        public HashSet<Ingredient> Ingredients { get; set; }
+    }
+}
