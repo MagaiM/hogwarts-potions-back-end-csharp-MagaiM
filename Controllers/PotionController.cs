4b1b0daa87baffa7c2ab6bf3d207c2161e693bdf
MagaiM <magai.marton@gmail.com>
2022-06-30 20:05:30 +0200

    Rename BrewPotion endpoint to AddPotion endpoint, Add new BrewPotion Endpoint

--- Controllers/PotionController.cs
+++ Controllers/PotionController.cs
@@ -23,15 +23,21 @@ namespace HogwartsPotions.Controllers
         }
 
         [HttpPost]
-        public async Task<Potion> BrewPotion(CreatePotion potion)
+        public async Task<Potion> AddPotion(CreatePotion potion)
         {
-            return await _context.BrewPotion(potion);
+            return await _context.AddPotion(potion);
         }
 
         [HttpGet("/potions/{studentId:long}")]
         public async Task<List<Potion>> GetAllPotionByStudent(long studentId)
         {
             return await _context.GetAllPotionByStudent(studentId);
+        }
+
+        [HttpPost("/potions/brew")]
+        public async Task<Potion> BrewPotion(BrewPotion potion)
+        {
+            return await _context.BrewPotion(potion);
         }
     }
 }
