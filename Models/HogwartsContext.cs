892d012ad4affd9c1d8a507a8f2dd54ce27ed8ea
MagaiM <magai.marton@gmail.com>
2022-06-30 19:02:29 +0200

    Add BrewPotion methods to context

--- Models/HogwartsContext.cs
+++ Models/HogwartsContext.cs
@@ -31,15 +31,16 @@ namespace HogwartsPotions.Models
             modelBuilder.Entity<Potion>().ToTable("Potion");
         }
 
-        public async Task AddRoom(Room room)
+        public Task<Student> GetStudent(long studentId)
         {
-            Rooms.Add(room);
-            await SaveChangesAsync();
+            var student = Students.Include(student => student.Room).ToListAsync().Result
+                .FirstOrDefault(student => student.ID == studentId);
+            return Task.FromResult(student);
         }
 
         public Task<Room> GetRoom(long roomId)
         {
-            var room = Rooms.Include(room => room.Residents).ToListAsync().Result.First(room => room.ID == roomId);
+            var room = Rooms.Include(room => room.Residents).ToListAsync().Result.FirstOrDefault(room => room.ID == roomId);
 
             return Task.FromResult(room);
         }
@@ -49,6 +50,12 @@ namespace HogwartsPotions.Models
             var rooms = Rooms.Include(room => room.Residents).ToListAsync().Result;
 
             return Task.FromResult(rooms);
+        }
+
+        public async Task AddRoom(Room room)
+        {
+            Rooms.Add(room);
+            await SaveChangesAsync();
         }
 
         public async Task UpdateRoom(long id, Room room)
@@ -75,16 +82,96 @@ namespace HogwartsPotions.Models
         public Task<List<Room>> GetRoomsForRatOwners()
         {
             var allRooms = GetAllRooms().Result;
-            var rooms = (from room in allRooms let ratable = room.Residents.All(roomResident => roomResident.PetType is not (PetType.Cat or PetType.Owl)) where ratable select room).ToList();
+            var rooms = (from room in allRooms
+                let ratable =
+                    room.Residents.All(roomResident => roomResident.PetType is not (PetType.Cat or PetType.Owl))
+                where ratable
+                select room).ToList();
 
             return Task.FromResult(rooms);
         }
 
         public Task<List<Potion>> GetAllPotion()
         {
-            var potions = Potions.Include(potion => potion.Ingredients).Include(potion => potion.Recipe).ToListAsync().Result;
+            var potions = Potions.Include(potion => potion.Ingredients).Include(potion => potion.Recipe)
+                .Include(potion => potion.Student).ToListAsync().Result;
 
             return Task.FromResult(potions);
+        }
+
+        public async Task BrewPotion(CreatePotion createPotion)
+        {
+            var student = GetStudent(createPotion.StudentId).Result;
+            var recipe = new Recipe();
+            var ingredients = GetIngredients(createPotion.Ingredients);
+            BrewingStatus brewingStatus;
+
+            if (createPotion.Ingredients.Count <= 0)
+            {
+                return;
+            }
+            if (createPotion.Ingredients.Count < MaxIngredientsForPotions)
+            {
+                brewingStatus = BrewingStatus.Brew;
+                recipe = default;
+            }
+            else if (createPotion.Ingredients.Count > MaxIngredientsForPotions)
+            {
+                return;
+            }
+            else
+            {
+                var allRecipes = Recipes.Include(r => r.Ingredients).Include(r => r.Student).ToListAsync().Result;
+                if (allRecipes.Any(r => r.Ingredients.SetEquals(ingredients)))
+                {
+                    brewingStatus = BrewingStatus.Replica;
+                    recipe = allRecipes.First(r => r.Ingredients.SetEquals(ingredients));
+                }
+                else
+                {
+                    brewingStatus = BrewingStatus.Discovery;
+                    var recipeName = GenerateRecipeName(student);
+                    recipe = new Recipe
+                    {
+                        Ingredients = ingredients,
+                        Name = recipeName,
+                        Student = student
+                    };
+                    Recipes.Add(recipe);
+                    await SaveChangesAsync();
+                }
+            }
+
+            var potion = new Potion
+            {
+                Ingredients = ingredients,
+                Student = student,
+                BrewingStatus = brewingStatus,
+                Name = createPotion.Name,
+                Recipe = recipe
+            };
+
+            Potions.Add(potion);
+            await SaveChangesAsync();
+        }
+
+        private HashSet<Ingredient> GetIngredients(HashSet<Ingredient> createPotionIngredients)
+        {
+            var allIngredients = Ingredients.ToListAsync().Result;
+            var ingredients = new HashSet<Ingredient>();
+            foreach (var potionIngredient in createPotionIngredients)
+            {
+                ingredients.Add(allIngredients.Any(i => i.Name == potionIngredient.Name)
+                    ? allIngredients.First(i => i.Name == potionIngredient.Name)
+                    : potionIngredient);
+            }
+            return ingredients;
+        }
+
+        private string GenerateRecipeName(Student student)
+        {
+            var studentRecipes = Recipes.ToListAsync().Result.Where(recipe => recipe.Student.ID == student.ID).ToList();
+            return $"{student.Name}'s discovery #{studentRecipes.Count + 1}";
         }
     }
 }
