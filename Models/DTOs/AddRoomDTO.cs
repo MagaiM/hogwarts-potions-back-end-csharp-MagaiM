6b5f4950e91eb71841f6e764ec1083557aa937c9
MagaiM <magai.marton@gmail.com>
2022-11-08 16:30:40 +0100

    Add AddRoomDTO.cs

---
+++ Models/DTOs/AddRoomDTO.cs
@@ -0,0 +1,12 @@
+﻿using HogwartsPotions.Models.Enums;
+
+namespace HogwartsPotions.Models.DTOs
+{
+    public class AddRoomDTO
+    {
+
+        public long Capacity { get; set; }
+
+        public HouseType RoomHouseType { get; set; }
+    }
+}
