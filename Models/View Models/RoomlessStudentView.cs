23ac8d04257d3757d1d28a6c4c83c053ce546e14
MagaiM <magai.marton@gmail.com>
2023-01-18 15:24:15 +0100

    Add RoomlessStudentView.cs

---
+++ Models/View Models/RoomlessStudentView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Models.Enums;
+
+namespace HogwartsPotions.Models.View_Models
+{
+    public class RoomlessStudentView
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public HouseType HouseType { get; set; }
+        public PetType PetType { get; set; }
+        public List<Room> AvailableRooms { get; set; }
+    }
+}
