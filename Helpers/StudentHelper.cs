24d6c5ce8fb8fd8e7120fb81a463a936ac94eb79
MagaiM <magai.marton@gmail.com>
2023-01-18 14:43:59 +0100

    Add StudentHelper.cs to generate random house types

---
+++ Helpers/StudentHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using HogwartsPotions.Models.Enums;
+
+namespace HogwartsPotions.Helpers
+{
+    public class StudentHelper
+    {
+        private readonly Random _random = new();
+
+        public HouseType GetRandomHouseType(HouseType preferredHouseType)
+        {
+            var randomHouseType = _random.Next(0, 6);
+            if (randomHouseType >= 4) return preferredHouseType;
+            return (HouseType) randomHouseType;
+        }
+
+        public HouseType GetRandomHouseType()
+        {
+            var randomHouseType = _random.Next(0, 4);
+            return (HouseType) randomHouseType;
+        }
+    }
+}
