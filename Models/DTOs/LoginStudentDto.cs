dba4f475aa12002058c8110bf4338762dd861884
MagaiM <magai.marton@gmail.com>
2023-01-18 15:20:20 +0100

    Add LoginStudentDto.cs

---
+++ Models/DTOs/LoginStudentDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HogwartsPotions.Models.DTOs
+{
+    public class LoginStudentDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
