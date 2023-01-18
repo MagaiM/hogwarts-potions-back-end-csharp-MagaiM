23ba906ad66e6b973933627d856a021d8539f00d
MagaiM <magai.marton@gmail.com>
2023-01-18 15:24:37 +0100

    Add StudentView.cs

---
+++ Models/View Models/StudentView.cs
@@ -0,0 +1,14 @@
+﻿using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Models.Enums;
+
+namespace HogwartsPotions.Models.View_Models
+{
+    public class StudentView
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public HouseType HouseType { get; set; }
+        public PetType PetType { get; set; }
+        public Room Room { get; set; }
+    }
+}
