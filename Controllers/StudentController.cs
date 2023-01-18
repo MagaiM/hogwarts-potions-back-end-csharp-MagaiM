f117f74bac7920f7dea4a9772c8ed3eff23a8550
MagaiM <magai.marton@gmail.com>
2023-01-18 15:41:18 +0100

    Add RoomService, UserManager, SignInManager injections to StudentController.cs, Add AddStudentFirstStep endpoint, Add AddStudentSecondStep endpoint, Modify GetStudentById to match the newIdentityUser (string) id, Add LogIn endpoint, Refactor Id-s from long to string type to match IdentityUser id

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,7 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using HogwartsPotions.Models.DTOs;
 using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Models.View_Models;
 using HogwartsPotions.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HogwartsPotions.Controllers
@@ -10,10 +15,16 @@ namespace HogwartsPotions.Controllers
     public class StudentController : ControllerBase
     {
         private readonly StudentService _studentService;
+        private readonly RoomService _roomService;
+        private readonly UserManager<Student> _userManager;
+        private readonly SignInManager<Student> _signInManager;
 
-        public StudentController(StudentService studentService)
+        public StudentController(StudentService studentService, RoomService roomService, UserManager<Student> userManager, SignInManager<Student> signInManager)
         {
             _studentService = studentService;
+            _roomService = roomService;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         [HttpGet]
@@ -22,28 +33,112 @@ namespace HogwartsPotions.Controllers
             return await _studentService.GetAllStudents();
         }
 
-        [HttpPost]
-        public async Task AddStudent([FromBody] Student student)
+        [HttpPost("/student/first-step")]
+        public async Task<IActionResult> AddStudentFirstStep([FromBody] AddStudentInProgressDto student)
         {
-            await _studentService.AddStudent(student);
+            try
+            {
+                if (_studentService.IsStudentNameAlreadyExists(student.UserName).Result)
+                    return BadRequest("Student with this name already exists!");
+                var roomlessStudent = await _studentService.AddRoomlessStudent(student);
+                var availableRooms = _roomService.GetAvailableRooms(roomlessStudent);
+                if (!availableRooms.Result.Any())
+                {
+                    var newRoomDto = new AddRoomDto()
+                    {
+                        Capacity = 10,
+                        RoomHouseType = roomlessStudent.HouseType
+                    };
+                    var newRoom = await _roomService.AddRoom(newRoomDto);
+                    availableRooms.Result.Add(newRoom);
+                }
+
+                var roomlessStudentView = new RoomlessStudentView()
+                {
+                    Id = roomlessStudent.Id,
+                    UserName = roomlessStudent.UserName,
+                    HouseType = roomlessStudent.HouseType,
+                    PetType = roomlessStudent.PetType,
+                    AvailableRooms = availableRooms.Result
+                };
+                return Ok(roomlessStudentView);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return BadRequest("Something went wrong");
+            }
         }
 
-        [HttpGet("/student/{id:long}")]
-        public async Task<Student> GetStudentById(long id)
+        [HttpPost("/student/second-step/{id}")]
+        public async Task<IActionResult> AddStudentSecondStep(string id, [FromBody] Room room)
         {
-            return await _studentService.GetStudent(id);
+            try
+            {
+                var validRoom = _roomService.GetValidRoom(room.Id, id).Result;
+                if (validRoom == null) return BadRequest("Invalid room was given!");
+                var student =  _studentService.GetStudentById(id).Result;
+                if (student == null) return BadRequest("Invalid student id! Or other failures occurred!");
+                if (student.HouseType != validRoom.RoomHouseType) return BadRequest("You can only select a room with the same House type as yours!");
+                await _studentService.AddStudentToRoom(id, validRoom);
+                var studentView = new StudentView
+                {
+                    Id = student.Id,
+                    UserName = student.UserName,
+                    HouseType = student.HouseType,
+                    PetType = student.PetType,
+                    Room = student.Room
+                };
+
+                return Ok(studentView);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return BadRequest("Something went wrong");
+            }
         }
 
-        [HttpPut("/student/{id:long}")]
-        public async Task UpdateStudentById(long id, [FromBody] Student updatedStudent)
+        [HttpGet("/student/{id}")]
+        public async Task<StudentView> GetStudentById(string id)
+        {
+            var student = await _studentService.GetStudentById(id);
+
+            var studentView = new StudentView()
+            {
+                Id = student.Id,
+                UserName = student.UserName,
+                HouseType = student.HouseType,
+                PetType = student.PetType,
+                Room = new Room()
+                {
+                    Id = student.Room.Id,
+                    RoomHouseType = student.Room.RoomHouseType,
+                    Capacity = student.Room.Capacity
+                }
+            };
+            return studentView;
+        }
+
+        [HttpPut("/student/{id}")]
+        public async Task UpdateStudentById(string id, [FromBody] Student updatedStudent)
         {
             await _studentService.UpdateStudent(id, updatedStudent);
         }
 
-        [HttpDelete("/student/{id:long}")]
-        public async Task DeleteStudentById(long id)
+        [HttpDelete("/student/{id}")]
+        public async Task DeleteStudentById(string id)
         {
             await _studentService.DeleteStudent(id);
+        }
+
+        [HttpPost("/login")]
+        public async Task<IActionResult> LogIn([FromBody] Student student)
+        {
+            //TODO implement login
+            await _signInManager.SignInAsync(student, isPersistent: true);
+            return Ok();
         }
     }
 }
