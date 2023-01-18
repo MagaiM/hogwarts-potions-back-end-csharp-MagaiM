a5fa2768bdda884140f4ad64a18343bf85b9599e
MagaiM <magai.marton@gmail.com>
2023-01-18 15:23:10 +0100

    Refactor rename ID to Id in Recipe.cs

--- Models/Entities/Recipe.cs
+++ Models/Entities/Recipe.cs
@@ -6,7 +6,7 @@ namespace HogwartsPotions.Models.Entities
     public class Recipe
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public long ID { get; set; }
+        public long Id { get; set; }
         public string Name { get; set; }
         public Student Student { get; set; }
         public HashSet<Ingredient> Ingredients { get; set; } = new HashSet<Ingredient>();
