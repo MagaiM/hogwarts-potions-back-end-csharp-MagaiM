7cdd1b89b1fc8df799f38009031b7edf8f9ddd52
MagaiM <magai.marton@gmail.com>
2023-01-19 14:48:51 +0100

    Add RegisterAdminDto.cs

---
+++ Models/DTOs/RegisterAdminDto.cs
@@ -0,0 +1,14 @@
+﻿#nullable enable
+using System.ComponentModel.DataAnnotations;
+
+namespace HogwartsPotions.Models.DTOs
+{
+    public class RegisterAdminDto
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string? UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
+    }
+}
