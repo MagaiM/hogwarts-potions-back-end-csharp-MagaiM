8cc3b7f7a9c561a34516e8e8675c614710d909f0
MagaiM <magai.marton@gmail.com>
2023-01-19 14:50:50 +0100

    Add Required atributes to UserName, Password in LoginStudentDto.cs

--- Models/DTOs/LoginStudentDto.cs
+++ Models/DTOs/LoginStudentDto.cs
@@ -1,8 +1,14 @@
-﻿namespace HogwartsPotions.Models.DTOs
+﻿#nullable enable
+using System.ComponentModel.DataAnnotations;
+
+namespace HogwartsPotions.Models.DTOs
 {
     public class LoginStudentDto
     {
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        [Required(ErrorMessage = "User Name is required")]
+        public string? UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
     }
 }
