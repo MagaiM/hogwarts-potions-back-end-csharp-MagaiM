d628329df5b6289e783fc6d5a6836dc14779c8c4
MagaiM <magai.marton@gmail.com>
2023-01-19 14:47:25 +0100

    Rename AddStudentInProgressDto to RegisterStudentDto, Add Required atribute to UserName and Password

---
+++ Models/DTOs/RegisterStudentDto.cs
@@ -0,0 +1,22 @@
+﻿#nullable enable
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using HogwartsPotions.Models.Enums;
+
+namespace HogwartsPotions.Models.DTOs
+{
+    public class RegisterStudentDto
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string? UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
+
+        [DefaultValue(PetType.None)]
+        public PetType PetType { get; set; }
+
+        public HouseType ?PreferredHouseType { get; set; }
+
+    }
+}
