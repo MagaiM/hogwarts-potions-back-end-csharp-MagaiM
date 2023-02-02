3a480aa5bf4293a1f6394d6a3fdc9206ade33ccb
MagaiM <magai.marton@gmail.com>
2023-02-02 14:29:46 +0100

    Add constructors to StudentView

--- Models/View Models/StudentView.cs
+++ Models/View Models/StudentView.cs
@@ -1,4 +1,5 @@
-﻿using HogwartsPotions.Models.Entities;
+﻿using System.Collections.Generic;
+using HogwartsPotions.Models.Entities;
 using HogwartsPotions.Models.Enums;
 
 namespace HogwartsPotions.Models.View_Models
@@ -10,5 +11,18 @@ namespace HogwartsPotions.Models.View_Models
         public HouseType HouseType { get; set; }
         public PetType PetType { get; set; }
         public Room Room { get; set; }
+
+        public StudentView()
+        {
+        }
+
+        public StudentView(Student student)
+        {
+            Id = student.Id;
+            UserName = student.UserName;
+            HouseType = student.HouseType;
+            PetType = student.PetType;
+            Room = student.Room;
+        }
     }
 }
